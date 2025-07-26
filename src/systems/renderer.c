bd80f128d7e319330beefb427f4324a197fb7f84
ftambler <federicotambler@gmail.com>
2025-07-26 13:50:31 -0300

    drawBlock wrapper

--- src/systems/renderer.c
+++ src/systems/renderer.c
@@ -14,41 +14,45 @@ void deInitRenderer() {
     UnloadTexture(grassSprite);
 }
 
-void drawPlayer(Vector2 position) {
-    DrawTexturePro(playerSprite, (Rectangle){ 0.0f, 0.0f, playerSprite.height, playerSprite.width }, 
-        (Rectangle){ position.x, position.y, 32, 32 }, 
+void drawPlayer(Vector2 position, int playerSize) {
+    DrawTexturePro(playerSprite, 
+        (Rectangle){ 0.0f, 0.0f, playerSprite.height, playerSprite.width }, 
+        (Rectangle){ position.x, position.y, playerSize, playerSize }, 
+        (Vector2){0, 0}, 0.0f, WHITE);
+}
+
+void drawBlock(Texture2D drawTexture, Vector2 position, int tileSize) {
+    DrawTexturePro(drawTexture, 
+        (Rectangle){ 0.0f, 0.0f, grassSprite.height, grassSprite.width }, 
+        (Rectangle){ position.x * tileSize, position.y * tileSize, tileSize, tileSize }, 
         (Vector2){0, 0}, 0.0f, WHITE);
 }
 
 void drawLevel(Level* level) {
     GameObject* roomObjects = level->objects;
     int tileSize = 20;
+
     for(int i = 0; i < level->objectCount; i++) {
-        
-        if(roomObjects[i].type == CHECKPOINT) {
-            if(roomObjects[i].checkpoint.isActive)  
-                DrawRectangle(roomObjects[i].position.x * tileSize, roomObjects[i].position.y * tileSize, tileSize, tileSize, GREEN);
-            else 
-                DrawRectangle(roomObjects[i].position.x * tileSize, roomObjects[i].position.y * tileSize, tileSize, tileSize, RED);
-        }
+        switch(roomObjects[i].type) {
+            case CHECKPOINT:
+                DrawRectangle(roomObjects[i].position.x * tileSize, roomObjects[i].position.y * tileSize, tileSize, tileSize, roomObjects[i].checkpoint.isActive ? GREEN : RED);
+                break;
+            
+            case OBJECT:
+                switch (roomObjects[i].object.textureId) {
+                    case GRASS:
+                        drawBlock(grassSprite, roomObjects[i].position, 20);
+                        break;
+                        
+                    default:
+                        drawBlock(grassSprite, roomObjects[i].position, 20);
+                        break;
+                }
+                break;
 
-        if(roomObjects[i].type != OBJECT) continue;
-
-        Texture2D drawTexture;
-        switch (roomObjects[i].object.textureId)
-        {
-        case GRASS:
-            drawTexture = grassSprite;
-            break;
-        
         default:
-            drawTexture = grassSprite;
             break;
         }
-        
-        DrawTexturePro(drawTexture, (Rectangle){ 0.0f, 0.0f, grassSprite.height, grassSprite.width }, 
-            (Rectangle){ roomObjects[i].position.x * tileSize, roomObjects[i].position.y * tileSize, tileSize, tileSize }, 
-            (Vector2){0, 0}, 0.0f, WHITE);
 
     }
 }
