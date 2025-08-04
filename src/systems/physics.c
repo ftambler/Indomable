bdd9d2ffd8429bf5c07039c34ff9cdc3449fc241
ftambler <federicotambler@gmail.com>
2025-08-04 15:38:33 -0300

    friction change

--- src/systems/physics.c
+++ src/systems/physics.c
@@ -1,7 +1,7 @@
 #include "physics.h"
 
 float gravity = 500;
-float groundFriction = -0.6f;
+float groundFriction = 0.6f;
 
 bool checkSqSqCollision(Vector2 obj1, int size1, Vector2 obj2, int size2) {
     return !(obj1.x > obj2.x * size2 + size2 ||
