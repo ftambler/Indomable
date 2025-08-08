c0a98c609e0f674c9ef2fa8656b89f758798dbd9
ftambler <federicotambler@gmail.com>
2025-08-08 17:35:59 -0300

    removed old collision checker

--- include/physics/physics.h
+++ include/physics/physics.h
@@ -5,5 +5,3 @@
 // Consts
 extern float gravity;
 extern float groundFriction;
-
-bool checkSqSqCollision(Vector2 obj1, int size1, Vector2 obj2, int size2);
--- src/physics/physics.c
+++ src/physics/physics.c
@@ -2,11 +2,3 @@
 
 float gravity = 500;
 float groundFriction = 0.6f;
-
-bool checkSqSqCollision(Vector2 obj1, int size1, Vector2 obj2, int size2) {
-    return !(obj1.x > obj2.x * size2 + size2 ||
-        obj1.x + size1 < obj2.x * size2 ||
-        obj1.y > obj2.y * size2 + size2 ||
-        obj1.y + size1 < obj2.y * size2
-    );
-}
